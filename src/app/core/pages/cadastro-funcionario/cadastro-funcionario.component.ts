ee99d15cd56d935f6f27adf0d470981d02b0e454
RaLiotti <rafa.liotti@gmail.com>
2021-10-21 12:44:00 -0300

    Inclusao de cadastro de funcionario

---
+++ src/app/core/pages/cadastro-funcionario/cadastro-funcionario.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Funcionario } from 'src/app/core/domain/funcionario';
+import { FuncionarioService } from '../../shared/funcionario.service';
+
+@Component({
+  selector: 'app-cadastro-funcionario',
+  templateUrl: './cadastro-funcionario.component.html',
+  styleUrls: ['./cadastro-funcionario.component.scss']
+})
+export class CadastroFuncionarioComponent implements OnInit {
+
+
+  //funcionario: Funcionario[] | undefined;
+
+  funcionarios: Funcionario[] = [];
+
+  funcionario = new Funcionario;
+
+  exibir_carregar = false;
+
+
+  constructor(
+    public funcionarioService: FuncionarioService,
+  ) { }
+
+  ngOnInit() {
+    this.funcionarios
+    this.carregarFuncionarios();
+    
+  }
+
+  form = new FormGroup({
+    nome: new FormControl(),
+    email: new FormControl(),
+    cpf: new FormControl()
+  });
+
+  cadastrarFuncionario() {
+
+    this.funcionario.nome = this.form.value.nome;
+    this.funcionario.email = this.form.value.email;
+    this.funcionario.cpf = this.form.value.cpf;
+
+    debugger;
+    console.log(this.funcionario);
+
+    this.funcionarioService.setCadastrarFuncionario(this.funcionario).subscribe(
+      success => console.log('sucesso'),
+      error => console.log(error),
+      () => console.log('completo')
+     );
+
+  }
+
+
+  carregarFuncionarios(){
+    this.exibir_carregar = true;
+    this.funcionarioService.getListarFuncionarios().subscribe(data => this.funcionarios = data);
+    this.exibir_carregar = false;
+  }
+
+}
---
+++ src/app/core/shared/funcionario.service.ts
@@ -0,0 +1,38 @@
+import { Injectable  } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Funcionario } from '../domain/funcionario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FuncionarioService {
+
+  funcionarios = '/api/funcionario/funcionarios';
+
+  cadastrarFuncionario = '/api/funcionario/cadastrar';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+
+constructor(private httpClient: HttpClient) { }
+
+
+
+
+public getListarFuncionarios(): Observable<Funcionario[]>{
+  return this.httpClient.get<Funcionario[]>(this.funcionarios);
+}
+
+public setCadastrarFuncionario(funcionario: Funcionario){
+
+  debugger;
+  return this.httpClient.post(this.cadastrarFuncionario, funcionario);
+
+}
+
+}
